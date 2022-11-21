b768d1191d3c7c88fa510ac9aef8580be3025810
Mo 2 <mo2@Mohammads-MacBook-Pro.local>
2022-11-20 20:56:20 -0500

    Reproduce tsx dotenv loading bug

---
+++ external.ts
@@ -0,0 +1,3 @@
+export const EXTERNAL_IMPORT = 1;
+
+console.log(process.env.TSX_TEST);
---
+++ main.ts
@@ -0,0 +1,7 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+import { EXTERNAL_IMPORT } from "./external";
+
+console.log(EXTERNAL_IMPORT);
